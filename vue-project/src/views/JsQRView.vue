c4cdaa7b7c3bd337916638b1937fd80719e3c6b6
hidepy <hidork0222@yahoo.co.jp>
2024-03-17 20:04:40 +0900

    vue project作成しただけ

---
+++ vue-project/src/views/JsQRView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import jsQR from 'jsqr';
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+const count = ref<number>(0); // ジェネリクスで型注釈
+
+const user: User = reactive({} as User);
+
+// TODO: https://qiita.com/U_sagi/items/12cc39487a863e0136a0 , https://zenn.dev/sdkfz181tiger/articles/096dfb74d485db , https://qiita.com/kan_dai/items/4331aae12f5f2d3ad18d を使って実装する
+
+
+const setUser = ():void=> {
+  user.firstName = 'John'
+  user.lastName = 'Doe'
+  user.age = 25
+}
+</script>
+
+<template>
+  <h1>RefComponent</h1>
+  {{ count }}
+  <button @click="count++">クリック</button>
+
+  <div>FullName: {{ `${user.firstName} ${user.lastName}` }}</div>
+
+  <button @click="setUser">set user</button>
+
+</template>
